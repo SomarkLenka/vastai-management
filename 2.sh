af9545506539c0beef1ca90f34eb3cf4d58816d5
Somark Lenka <lenkasomark@gmail.com>
2024-10-16 15:44:21 -0400

    Create 2.sh

---
+++ 2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt install build-essential -y
+sudo add-apt-repository ppa:graphics-drivers/ppa -y
+sudo apt update
+sudo apt install nvidia-driver-560  -y    # assuming the latest is 560
+
+#Remove unattended-upgrades Package so that the dirver don't upgrade when you have clients
+sudo apt purge --auto-remove unattended-upgrades -y
+sudo systemctl disable apt-daily-upgrade.timer
+sudo systemctl mask apt-daily-upgrade.service 
+sudo systemctl disable apt-daily.timer
+sudo systemctl mask apt-daily.service

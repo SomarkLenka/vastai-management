20bf0d3f8957745696e67441e5a96c0d11947e16
Somark Lenka <lenkasomark@gmail.com>
2025-02-19 19:38:00 -0500

    Delete 1.sh
    Cleanup finished

--- 1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt install update-manager-core -y
-sudo apt install --install-recommends linux-generic-hwe-22.04 -y
